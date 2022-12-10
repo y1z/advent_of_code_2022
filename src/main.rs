f9e02af3588c7880012f954f7959007b99e451a2
y1z <whyis2cool@gmail.com>
2022-12-10 13:13:18 -0600

    Starts on day 1

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+/**
+* code made for the advent of code event 
+*
+* Yhaliff said Barraza 
+*
+*
+*
+*/
+
+
+
+
+fn main() {
+    println!("Hello, world!"); 
+    let number :i32 = 69696;
+    println!("{:x}",number);
+}
